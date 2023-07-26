b2e9cb22176ede78e91d3d7ac025c5c962efcec7
roy4801 <a82611141@gmail.com>
2023-07-27 01:59:01 +0800

    Switch deploy place from separate repo to gh-pages branch

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 [[ "$1" == "" ]] && MSG=update || MSG=$1
 
-cp -rf ./public/* ../roy4801.github.io/
-pushd ../roy4801.github.io/ > /dev/null
+# cp -rf ./public/* ../roy4801.github.io/
+cp -rf ./public/* .deploy_git/
+pushd .deploy_git/ > /dev/null
 
 git add .
 git commit -m "$(date +"%Y/%m/%d %H:%M:%S") $1"
-git push origin master
+git push origin gh-pages
 
 popd > /dev/null
